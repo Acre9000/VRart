9f21a53623bd598876f6466d1f8387eec8de12ba
SaraLisinka <katslalisinka@gmail.com>
2023-05-10 10:04:51 +0200

    main merge

--- Assets/Bakery/ftLocalStorage.cs
+++ Assets/Bakery/ftLocalStorage.cs
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 using UnityEngine;
 using System.Collections.Generic;
 
@@ -14,3 +15,21 @@ public class ftLocalStorage : ScriptableObject
     public List<int> modifiedAssetPaddingHash = new List<int>();
 }
 
+=======
+using UnityEngine;
+using System.Collections.Generic;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public class ftLocalStorage : ScriptableObject
+{
+    [SerializeField]
+    public List<string> modifiedAssetPathList = new List<string>(); // marks model as processed
+
+    [SerializeField]
+    public List<int> modifiedAssetPaddingHash = new List<int>();
+}
+
+>>>>>>> main
